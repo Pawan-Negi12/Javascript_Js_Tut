d03d16023acb0849b4427b018a4d28e8350f37ff
Pawan-Negi12 <negipawan787@gmail.com>
2026-02-03 23:14:54 +0530

    DOM

---
+++ 06_dom/script.js
@@ -0,0 +1,18 @@
+// console.log('Js Running');
+
+const parentDiv = document.querySelector('.parent');
+// console.log(parentDiv);
+// console.log(parentDiv.children);
+// console.log(parentDiv.children[1].innerHTML);
+
+// for (let i = 0; i < parentDiv.children.length; i++) {
+//     const element = parentDiv.children[i].innerHTML;
+//     console.log(element);
+// }
+
+const dayOne = document.querySelector('.day');
+// console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling);
+
+console.log(parentDiv.childNodes);
+
